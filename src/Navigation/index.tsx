8b27a8b331e4f10e164758b740cc0115b1bf07b4
aditya-sps <152249472+aditya-sps@users.noreply.github.com>
2024-05-06 11:11:03 +0530

    Initial Setup

---
+++ src/Navigation/index.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Dashboard from '../screens/Dashboard';
+import Home from '../screens/Home';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
+
+export default Navigation;
+
+const styles = StyleSheet.create({});
